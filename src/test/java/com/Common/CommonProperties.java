595cbbdc141a29929ed7db69a9806ff032cf12df
张世强 <zhangshiqiang.sh@superjia.com>
2017-08-24 17:03:40 +0800

    config

--- src/test/java/com/Common/CommonProperties.java
+++ src/test/java/com/Common/CommonProperties.java
@@ -10,7 +10,7 @@ import com.qiang.utils.FitnesseUtils;
 import com.qiang.utils.UseProperties;
 
 public class CommonProperties {
-	String mobile = "17455223369";
+	String mobile = "17455223368";
 	String url = new UserCommon().getHost();
 	String verify;
 	String result;
