0820f346458345ae8cee2326dff2198fff14029a
张世强 <zhangshiqiang.sh@superjia.com>
2017-08-31 18:49:21 +0800

    消息通知

--- src/test/java/com/Demo/test/NoticeRequestTest.java
+++ src/test/java/com/Demo/test/NoticeRequestTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 
 import com.Common.UserCommon;
 import com.Demo.NoticeRequest;
+import com.qiang.db.slim.DbSlimSelectQuery;
 import com.qiang.utils.CommonUtils;
 import com.qiang.utils.UseProperties;
 
@@ -23,11 +24,17 @@ public class NoticeRequestTest {
   @Test
   public void getNoticeResponse1() {
     String result = new NoticeRequest().getNoticeResponse(uticket, "1");
-    Assert.assertEquals(CommonUtils.parseJson("errorCode", result), "0");
+    
+    //查询用户未读消息数目SELECT COUNT(*) FROM iwnotice_beta.iw_notice_remind WHERE receiveId=2726861 and status = 0;
+    DbSlimSelectQuery db = new DbSlimSelectQuery("121.40.127.92", "3309", "qa_tmp", "mjl84_)l23jda0l_23");
+	String num = db.queryByName("COUNT(*)","SELECT COUNT(*) FROM iwnotice_beta.iw_notice_remind WHERE receiveId="+userId+" and status = 0;");
+    Assert.assertEquals(CommonUtils.parseJson("remindNum", result), num);
   }
   @Test
   public void getNoticeResponse2() {
     String result = new NoticeRequest().getNoticeResponse(uticket);
-    Assert.assertEquals(CommonUtils.parseJson("errorCode", result), "0");
+    DbSlimSelectQuery db = new DbSlimSelectQuery("121.40.127.92", "3309", "qa_tmp", "mjl84_)l23jda0l_23");
+	String num = db.queryByName("COUNT(*)","SELECT COUNT(*) FROM iwnotice_beta.iw_notice_remind WHERE receiveId="+userId+" and status = 0;");
+    Assert.assertEquals(CommonUtils.parseJson("remindNum", result), num);
   }
 }
